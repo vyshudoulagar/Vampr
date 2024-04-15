d70369513e7f9ea36c9f4d77f63c584de4c3c23e
WishNavy <du.vyshu@gmail.com>
2024-04-15 13:48:07 -0700

    Add functions for traversal

--- vampire.js
+++ vampire.js
@@ -1,43 +1,108 @@
 class Vampire {
-  constructor(name, yearConverted) {
-    this.name = name;
-    this.yearConverted = yearConverted;
-    this.offspring = [];
-    this.creator = null;
-  }
+    constructor(name, yearConverted) {
+        this.name = name;
+        this.yearConverted = yearConverted;
+        this.offspring = [];
+        this.creator = null;
+    }
 
-  /** Simple tree methods **/
+    /** Simple tree methods **/
 
-  // Adds the vampire as an offspring of this vampire
-  addOffspring(vampire) {
+    // Adds the vampire as an offspring of this vampire
+    addOffspring(vampire) {
+        this.offspring.push(vampire);
+        vampire.creator = this;
+    }
 
-  }
+    // Returns the total number of vampires created by that vampire
+    get numberOfOffspring() {
+        return this.offspring.length;
+    }
 
-  // Returns the total number of vampires created by that vampire
-  get numberOfOffspring() {
+    // Returns the number of vampires away from the original vampire this vampire is
+    get numberOfVampiresFromOriginal() {
+        let numberOfVampires = 0;
+        let currentVampire = this;
 
-  }
+        while (currentVampire.creator) {
+            currentVampire = currentVampire.creator;
+            numberOfVampires++;
+        }
+        return numberOfVampires;
+    }
 
-  // Returns the number of vampires away from the original vampire this vampire is
-  get numberOfVampiresFromOriginal() {
+    // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
+    isMoreSeniorThan(vampire) {
+        if (this.numberOfVampiresFromOriginal > vampire.numberOfVampiresFromOriginal) {
+            return false;
+        }
+        if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+            return true;
+        }
+    }
 
-  }
+    /** Stretch **/
 
-  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-  isMoreSeniorThan(vampire) {
+    // Returns the closest common ancestor of two vampires.
+    // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
+    // For example:
+    // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
+    // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
+    closestCommonAncestor(vampire) {
+        let commonAncestor;
+        if (!this.creator) {
+            return commonAncestor = this;
+        }
+        if (!vampire.creator) {
+            return commonAncestor = vampire;
+        }
+        if (this === vampire) {
+            return commonAncestor = vampire;
+        }
+        if (vampire.offspring.includes(this)) {
+            return commonAncestor = vampire;
+        }
+        if (this.creator = vampire.creator) {
+            return commonAncestor = this.creator;
+        }
+    }
 
-  }
+    // Returns the vampire object with that name, or null if no vampire exists with that name
+    vampireWithName(name) {
+        if (this.name === name) {
+            return this;
+        }
+        for (const vampire of this.offspring) {
+            const foundVampire = vampire.vampireWithName(name);
+            if(foundVampire) {
+                return foundVampire;
+            }
+        }
+        return null;
+    }
 
-  /** Stretch **/
+    // Returns the total number of vampires that exist
+    get totalDescendents() {
+        let numberOfVampires = 0;
+        for(const vampire of this.offspring){
+            numberOfVampires++;
+            numberOfVampires += vampire.totalDescendents;
+        }
+        return numberOfVampires;
+    }
 
-  // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-
-  }
+    // Returns an array of all the vampires that were converted after 1980
+    get allMillennialVampires() {
+        let millennialVampires = [];
+        if(this.yearConverted > 1980){
+            millennialVampires.push(this);
+        }
+        for(const vampire of this.offspring){
+            const vampires = vampire.allMillennialVampires;
+            millennialVampires = millennialVampires.concat(vampires);
+        }
+        return millennialVampires;
+    }
 }
 
 module.exports = Vampire;
